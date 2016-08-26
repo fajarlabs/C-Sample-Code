3da7a1eca56f8ea2f8c4e58b7a79b7703526ad38
fajar <fajarrdp@gmail.com>
2016-08-26 10:57:50 +0700

    Edit Recursion

--- contoh_recursion.c
+++ contoh_recursion.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 int factorial(unsigned int i) {
-   if(i <= 1) {
+   if(i <= 1)
       return 1;
-   }
+   
    return i * factorial(i - 1);
 }
 
 int fibonaci(int i) {
-   if(i == 0) {
+   if(i == 0)
       return 0;
-   }
 	
-   if(i == 1) {
+   if(i == 1)
       return 1;
-   }
+  
    return fibonaci(i-1) + fibonaci(i-2);
 }
 
@@ -25,8 +24,7 @@ void contohRecursion() {
     printf("======================= \n");
     printf("FIBONACI ======================= \n");
     int j;
-    for (j = 0; j < 10; j++) {
+    for (j = 0; j < 10; j++)
         printf("%d\t", fibonaci(j));
-   	}
 
 }
